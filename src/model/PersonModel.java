ec53d6cd9a20d23a3c63b6434f6f93236a0d246f
Thomas Dutouquet <thomas@ocbase.com>
2025-01-09 19:50:31 +0100

    Bugfix

--- src/model/PersonModel.java
+++ src/model/PersonModel.java
@@ -5,11 +5,11 @@ import person.*;
 public class PersonModel {
 
     public Person setupPersonObject(int age) {
-        if (age < 13) {
+        if (age < 12) {
             return new Child();
-        } else if (age < 20) {
+        } else if (age < 18) {
             return new Teenager();
-        } else if (age < 65) {
+        } else if (age < 62) {
             return new Worker();
         } else {
             return new Retiree();
--- src/person/Adult.java
+++ src/person/Adult.java
@@ -16,7 +16,7 @@ public abstract class Adult extends Person {
 
     public String toString() {
         return super.toString() + " is an Adult and" +
-                (hasLicense ? " has a driver's license." : ", doesn't have a driver's license.");
+                (hasLicense ? " has a driver's license." : " doesn't have a driver's license.");
     }
 
     public boolean isHasLicense() {
